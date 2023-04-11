6b625034f7a3eee1b208011571181ed005308d3f
rafa2mgala <rmenmar264@g.educaand.es>
2023-04-11 14:23:44 +0200

    Ejemplo con expresiones básicas

--- DamThymeleafDemo/src/main/java/dam/thymeleaf/demo/MainController.java
+++ DamThymeleafDemo/src/main/java/dam/thymeleaf/demo/MainController.java
@@ -1,5 +1,10 @@
 package dam.thymeleaf.demo;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +14,57 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
 	@GetMapping({"/","/wellcome"})
 	public String wellcome(@RequestParam(name="nombre",
-		required=false, defaultValue=" Mundo")String nombre,
+	required=false, defaultValue=" Mundo")String nombre,
 			Model model) {
 		//añado todos los atributos que yo quiera
 		//que la plantilla utilice
 		model.addAttribute("nombre",nombre);
-		Producto producto = new Producto("C12345",2.34f);
+		Producto producto = new Producto("Kingston KVR677D2N5/1G",
+				"7 MHz DDR2 Non-ECC CL5 DIMM, 240-pin", 28.34f);
 		model.addAttribute("producto",producto);
 		return "index";
+	}
+	@GetMapping({"/opbasicas"})
+	public String opbasicas(Model model) {
+		model.addAttribute("htmltext", "<strong>Texto en negrita</strong>");
+		model.addAttribute("today", LocalDate.now());
+		model.addAttribute("nombre", "Rafa");
+		Producto producto = new Producto("Kingston KVR677D2N5/1G",
+				"7 MHz DDR2 Non-ECC CL5 DIMM, 240-pin", 28.34f);
+		model.addAttribute("producto",producto);
+		ArrayList<Producto> productos = new ArrayList<Producto>();
+		productos.add(producto);
+		model.addAttribute("lista", productos);
+		
+		HashMap<String,Producto> map = new HashMap<String,Producto>();
+		map.put("p4", producto);
+		model.addAttribute("map", map);
+		return "exbasicas";
+	}
+	/**@GetMapping({"/operadores"})
+	public String operadores(Model model) {
+		// Funciona igual que en ejemplos anteriores
+		model.addAttribute("nombre", nombre);
+		// Tomamos la fecha y hora actual del sistema
+		model.addAttribute("today", new Date());
+		// Obtenemos desde el servicio la instancia de un producto
+		model.addAttribute("producto", service.getProducto());
+		// Obtenemos desde el servicio un listado con varios productos
+		model.addAttribute("lista", service.getLista());
+		// Obtenemos desde el servicio un HashMap con un producto
+		model.addAttribute("map", service.getMap());
+		// Obtenemos del servicio la instancia de un producto sin alguno de sus valores		
+		model.addAttribute("producto2", service.getProductoSinAlgunosValores());
+	}**/
+	@GetMapping({"/producto/{id}"})
+	public String verProducto(Model model) {
+		Producto producto = new Producto("Kingston KVR677D2N5/1G",
+				"7 MHz DDR2 Non-ECC CL5 DIMM, 240-pin", 28.34f);
+		model.addAttribute(producto);
+		Map<String,Producto> productos = Collections.singletonMap("p4", 
+				producto);
+		productos.put("primero", producto);
+		model.addAttribute(productos);
+		return "producto";
 	}
 }
--- DamThymeleafDemo/src/main/java/dam/thymeleaf/demo/Producto.java
+++ DamThymeleafDemo/src/main/java/dam/thymeleaf/demo/Producto.java
@@ -1,24 +1,65 @@
 package dam.thymeleaf.demo;
 
 public class Producto {
-	private String referencia;
+	private String nombre;
+	private String descripcion;
 	private float precio;
+	private int numeroVotos;
+	private float porcentajeSatisfaccion;
 	
-	public Producto(String referencia, float precio) {
-		super();
-		this.referencia = referencia;
+	
+	public Producto(String nombre, String descripcion, float precio) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
 		this.precio = precio;
 	}
-	public String getReferencia() {
-		return referencia;
+	
+	public Producto(String nombre, String descripcion, float precio, int numeroVotos, float porcentajeSatisfaccion) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.numeroVotos = numeroVotos;
+		this.porcentajeSatisfaccion = porcentajeSatisfaccion;
 	}
-	public void setReferencia(String referencia) {
-		this.referencia = referencia;
+
+	public String getNombre() {
+		return nombre;
 	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
 	public float getPrecio() {
 		return precio;
 	}
+
 	public void setPrecio(float precio) {
 		this.precio = precio;
-	}	
+	}
+
+	public int getNumeroVotos() {
+		return numeroVotos;
+	}
+
+	public void setNumeroVotos(int numeroVotos) {
+		this.numeroVotos = numeroVotos;
+	}
+
+	public float getPorcentajeSatisfaccion() {
+		return porcentajeSatisfaccion;
+	}
+
+	public void setPorcentajeSatisfaccion(float porcentajeSatisfaccion) {
+		this.porcentajeSatisfaccion = porcentajeSatisfaccion;
+	}
 }
